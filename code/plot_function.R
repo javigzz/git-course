93ea83bbc7f97ac23f4530a11ff982944ef40b3d
Javier González <javigzz@yahoo.es>
2014-02-26 17:04:44 -0300

    function

--- code/plot_function.R
+++ code/plot_function.R
@@ -3,10 +3,10 @@ cars <- c(1, 3, 6, 4, 9)
 trucks <- c(2, 5, 4, 5, 12)
 
 # Graph cars using a y axis that ranges from 0 to 12
-plot(cars, type="o", col="blue", ylim=c(0,12))
+plot(cars, type="o", col="black", ylim=c(0,12))
 
 # Graph trucks with red dashed line and square points
-lines(trucks, type="o", pch=22, lty=2, col="red")
+lines(trucks, type="o", pch=22, lty=2, col="black")
 
 # Create a title with a red, bold/italic font
-title(main="Autos", col.main="red", font.main=4)
+title(main="Autos", col.main="black", font.main=4)
